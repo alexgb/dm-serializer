86f0bfe664e975b391f22150da6c263f3118bfd3
Alex Gibbons <alex.gibbons@gmail.com>
2010-08-31 18:40:17 -0700

    changed requires to work properly without gems

--- lib/dm-serializer.rb
+++ lib/dm-serializer.rb
@@ -1,4 +1,4 @@
-require 'dm-serializer/to_json'
-require 'dm-serializer/to_xml'
-require 'dm-serializer/to_yaml'
-require 'dm-serializer/to_csv'
+require File.join(File.dirname(__FILE__), 'dm-serializer', 'to_json')
+require File.join(File.dirname(__FILE__), 'dm-serializer', 'to_xml')
+require File.join(File.dirname(__FILE__), 'dm-serializer', 'to_yaml')
+require File.join(File.dirname(__FILE__), 'dm-serializer', 'to_csv')
